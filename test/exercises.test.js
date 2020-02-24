4f218a729cd6cc7b22fbd40dae21673f9aaaa82a
CheezItMan <chris@adadevelopersacademy.org>
2020-02-24 11:21:05 -0800

    convert to jest

--- test/exercises.test.js
+++ test/exercises.test.js
@@ -1,13 +1,12 @@
-const expect = require('chai').expect;
 const {
   grouped_anagrams,
   top_k_frequent_elements,
   valid_sudoku
 } = require('../lib/exercises');
 
-describe("exercises", function() {
-  describe("grouped_anagrams", function() {
-    it("will return [] for an empty array", function() {
+describe("exercises", function () {
+  describe("grouped_anagrams", function () {
+    it("will return [] for an empty array", function () {
       // Arrange
       const list = [];
 
@@ -15,15 +14,15 @@ describe("exercises", function() {
       expect(grouped_anagrams(list)).to.eql([]);
     });
 
-    it("will work for the README example", function() {
+    it("will work for the README example", function () {
       // Arrange
       const list = ["eat", "tea", "tan", "ate", "nat", "bat"];
 
       // Act
       const answer = grouped_anagrams(list);
       const expected_answer = [
-        ["ate","eat","tea"],
-        ["nat","tan"],
+        ["ate", "eat", "tea"],
+        ["nat", "tan"],
         ["bat"]
       ];
 
@@ -34,7 +33,7 @@ describe("exercises", function() {
       });
     });
 
-    it("will work for strings with no anagrams", function() {
+    it("will work for strings with no anagrams", function () {
       // Arrange
       const list = ["eat", "ear", "tar", "pop", "pan", "pap"];
 
@@ -57,14 +56,14 @@ describe("exercises", function() {
       });
     });
 
-    it("will work for strings that are all anagrams", function() {
+    it("will work for strings that are all anagrams", function () {
       // Arrange
       const list = ["eat", "tae", "tea", "eta", "aet", "ate"]
 
       // Act
       const answer = grouped_anagrams(list);
       const expected_answer = [
-        [ "aet", "ate", "eat", "eta", "tae", "tea"]
+        ["aet", "ate", "eat", "eta", "tae", "tea"]
       ];
 
       // Assert
@@ -75,20 +74,20 @@ describe("exercises", function() {
     });
   });
 
-  describe.skip("top_k_frequent_elements", function() {
-    it("works with example 1", function() {
+  describe.skip("top_k_frequent_elements", function () {
+    it("works with example 1", function () {
       // Arrange
-      const list = [1,1,1,2,2,3];
+      const list = [1, 1, 1, 2, 2, 3];
       const k = 2;
 
       // Act
       const answer = top_k_frequent_elements(list, k);
 
       // Assert
-      expect(answer.sort()).to.eql([1,2]);
+      expect(answer.sort()).to.eql([1, 2]);
     });
 
-    it("works with example 2", function() {
+    it("works with example 2", function () {
       // Arrange
       const list = [1];
       const k = 1;
@@ -100,7 +99,7 @@ describe("exercises", function() {
       expect(answer.sort()).to.eql([1]);
     });
 
-    it("will return [] for an empty array", function() {
+    it("will return [] for an empty array", function () {
       // Arrange
       const list = [];
       const k = 1;
@@ -112,7 +111,7 @@ describe("exercises", function() {
       expect(answer.sort()).to.eql([]);
     });
 
-    it("will work for an array with k elements all unique", function() {
+    it("will work for an array with k elements all unique", function () {
       // Arrange
       const list = [1, 2, 3];
       const k = 3;
@@ -124,7 +123,7 @@ describe("exercises", function() {
       expect(answer.sort()).to.eql([1, 2, 3]);
     });
 
-    it("will work for an array when k is 1 and several elements appear 1 time (HINT Pick the 1st one)", function() {
+    it("will work for an array when k is 1 and several elements appear 1 time (HINT Pick the 1st one)", function () {
       // Arrange
       const list = [1, 2, 3];
       const k = 1;
@@ -137,19 +136,19 @@ describe("exercises", function() {
     });
   });
 
-  describe.skip("valid sudoku", function() {
-    it("is not valid if a row has duplicate values", function() {
+  describe.skip("valid sudoku", function () {
+    it("is not valid if a row has duplicate values", function () {
       // Arrange
       const table = [
-        ["5","3",".",".",".",".",".",".","."],
-        [".",".",".",".",".",".",".",".","."],
-        [".",".",".",".",".",".",".",".","."],
-        [".",".",".",".","5","5",".",".","."],
-        [".",".",".",".",".",".",".",".","."],
-        [".",".",".",".",".",".",".",".","."],
-        [".",".",".",".",".",".",".",".","."],
-        [".",".",".",".",".",".",".",".","."],
-        [".",".",".",".",".",".",".",".","."]
+        ["5", "3", ".", ".", ".", ".", ".", ".", "."],
+        [".", ".", ".", ".", ".", ".", ".", ".", "."],
+        [".", ".", ".", ".", ".", ".", ".", ".", "."],
+        [".", ".", ".", ".", "5", "5", ".", ".", "."],
+        [".", ".", ".", ".", ".", ".", ".", ".", "."],
+        [".", ".", ".", ".", ".", ".", ".", ".", "."],
+        [".", ".", ".", ".", ".", ".", ".", ".", "."],
+        [".", ".", ".", ".", ".", ".", ".", ".", "."],
+        [".", ".", ".", ".", ".", ".", ".", ".", "."]
       ];
 
       // Act
@@ -159,18 +158,18 @@ describe("exercises", function() {
       expect(valid).to.be.false;
     });
 
-    it("is not valid if a column has duplicate values", function() {
+    it("is not valid if a column has duplicate values", function () {
       // Arrange
       const table = [
-        ["5",".",".",".",".",".",".",".","."],
-        ["2",".",".",".",".",".",".",".","."],
-        [".",".",".",".",".",".",".",".","."],
-        [".",".",".",".",".",".",".",".","."],
-        [".",".",".",".","4",".",".",".","."],
-        [".",".",".",".","4",".",".",".","."],
-        [".",".",".",".",".",".",".",".","."],
-        [".",".",".",".",".",".",".",".","."],
-        [".",".",".",".",".",".",".",".","."]
+        ["5", ".", ".", ".", ".", ".", ".", ".", "."],
+        ["2", ".", ".", ".", ".", ".", ".", ".", "."],
+        [".", ".", ".", ".", ".", ".", ".", ".", "."],
+        [".", ".", ".", ".", ".", ".", ".", ".", "."],
+        [".", ".", ".", ".", "4", ".", ".", ".", "."],
+        [".", ".", ".", ".", "4", ".", ".", ".", "."],
+        [".", ".", ".", ".", ".", ".", ".", ".", "."],
+        [".", ".", ".", ".", ".", ".", ".", ".", "."],
+        [".", ".", ".", ".", ".", ".", ".", ".", "."]
       ];
 
       // Act
@@ -180,18 +179,18 @@ describe("exercises", function() {
       expect(valid).to.be.false;
     });
 
-    it("works for the table given in the README", function() {
+    it("works for the table given in the README", function () {
       // Arrange
       const table = [
-        ["5","3",".",".","7",".",".",".","."],
-        ["6",".",".","1","9","5",".",".","."],
-        [".","9","8",".",".",".",".","6","."],
-        ["8",".",".",".","6",".",".",".","3"],
-        ["4",".",".","8",".","3",".",".","1"],
-        ["7",".",".",".","2",".",".",".","6"],
-        [".","6",".",".",".",".","2","8","."],
-        [".",".",".","4","1","9",".",".","5"],
-        [".",".",".",".","8",".",".","7","9"]
+        ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+        ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+        [".", "9", "8", ".", ".", ".", ".", "6", "."],
+        ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+        ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+        ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+        [".", "6", ".", ".", ".", ".", "2", "8", "."],
+        [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+        [".", ".", ".", ".", "8", ".", ".", "7", "9"]
       ];
 
       // Act
@@ -201,18 +200,18 @@ describe("exercises", function() {
       expect(valid).to.be.true;
     });
 
-    it("fails for the table given in the README", function() {
+    it("fails for the table given in the README", function () {
       // Arrange
       const table = [
-        ["8","3",".",".","7",".",".",".","."],
-        ["6",".",".","1","9","5",".",".","."],
-        [".","9","8",".",".",".",".","6","."],
-        ["8",".",".",".","6",".",".",".","3"],
-        ["4",".",".","8",".","3",".",".","1"],
-        ["7",".",".",".","2",".",".",".","6"],
-        [".","6",".",".",".",".","2","8","."],
-        [".",".",".","4","1","9",".",".","5"],
-        [".",".",".",".","8",".",".","7","9"]
+        ["8", "3", ".", ".", "7", ".", ".", ".", "."],
+        ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+        [".", "9", "8", ".", ".", ".", ".", "6", "."],
+        ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+        ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+        ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+        [".", "6", ".", ".", ".", ".", "2", "8", "."],
+        [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+        [".", ".", ".", ".", "8", ".", ".", "7", "9"]
       ];
 
       // Act
@@ -222,18 +221,18 @@ describe("exercises", function() {
       expect(valid).to.be.false;
     });
 
-    it("fails for a duplicate number in a sub-box", function() {
+    it("fails for a duplicate number in a sub-box", function () {
       // Arrange
       const table = [
-        ["5","3",".",".","7",".",".",".","."],
-        ["6",".","3","1","9","5",".",".","."],
-        [".","9","8",".",".",".",".","6","."],
-        ["8",".",".",".","6",".",".",".","3"],
-        ["4",".",".","8",".","3",".",".","1"],
-        ["7",".",".",".","2",".",".",".","6"],
-        [".","6",".",".",".",".","2","8","."],
-        [".",".",".","4","1","9",".",".","5"],
-        [".",".",".",".","8",".",".","7","9"]
+        ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+        ["6", ".", "3", "1", "9", "5", ".", ".", "."],
+        [".", "9", "8", ".", ".", ".", ".", "6", "."],
+        ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+        ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+        ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+        [".", "6", ".", ".", ".", ".", "2", "8", "."],
+        [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+        [".", ".", ".", ".", "8", ".", ".", "7", "9"]
       ];
 
       // Act
@@ -243,18 +242,18 @@ describe("exercises", function() {
       expect(valid).to.be.false;
     });
 
-    it("fails for a duplicate number in a bottom right sub-box", function() {
+    it("fails for a duplicate number in a bottom right sub-box", function () {
       // Arrange
       const table = [
-        ["5","3",".",".","7",".",".",".","."],
-        ["6",".","2","1","9","5",".",".","."],
-        [".","9","8",".",".",".",".","6","."],
-        ["8",".",".",".","6",".",".",".","3"],
-        ["4",".",".","8",".","3",".",".","1"],
-        ["7",".",".",".","2",".",".",".","6"],
-        [".","6",".",".",".",".","2","8","."],
-        [".",".",".","4","1","9","8",".","5"],
-        [".",".",".",".","8",".",".","7","9"]
+        ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+        ["6", ".", "2", "1", "9", "5", ".", ".", "."],
+        [".", "9", "8", ".", ".", ".", ".", "6", "."],
+        ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+        ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+        ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+        [".", "6", ".", ".", ".", ".", "2", "8", "."],
+        [".", ".", ".", "4", "1", "9", "8", ".", "5"],
+        [".", ".", ".", ".", "8", ".", ".", "7", "9"]
       ];
 
       // Act
